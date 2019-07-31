e8b510f61022da995d794e88b189e4c12260dfeb
Aqwam <maqwamhimami@gmail.com>
2019-07-31 15:11:11 +0700

    config edited

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 import "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyAoD6Q6L_vqIN1wBeQ8MVJyoNdfLMRbiOk",
-  authDomain: "mogo-bi.firebaseapp.com",
+  apiKey: "AIzaSyBKUVlm3KmuV_uAMWlJJ6AM8V82cU2wtgo",
+  authDomain: "hc-bi-984fa.firebaseapp.com",
   databaseURL: "https://mogo-bi.firebaseio.com",
-  projectId: "mogo-bi",
-  storageBucket: "mogo-bi.appspot.com",
-  messagingSenderId: "169620630404"
+  projectId: "hc-bi",
+  storageBucket: "gs://hc-bi-984fa.appspot.com",
+  messagingSenderId: "789290871363"
 };
 firebase.initializeApp(config);
 
